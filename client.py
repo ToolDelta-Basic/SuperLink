8dda955f53a28719748921b4f7c2391306cc26e6
SuperScriptPRC <2528622340@qq.com>
2024-07-03 10:57:58 +0800

    update IP

--- client.py
+++
@@ -1,8 +0,0 @@
-import websockets
-import asyncio
-
-async def a():
-    async with websockets.connect("ws://localhost:24013", extra_headers={"b":"2"}) as super_link: # type: ignore
-        await super_link.send("lala")
-
-asyncio.get_event_loop().run_until_complete(a())
---
+++ 扩展组件/基本-扩展查询/__init__.py
@@ -0,0 +1,12 @@
+from SuperLink import *
+
+__extension_data__ = {
+    "name": "基本-扩展查询",
+    "id": "extensions-check",
+    "version": (0, 0, 1),
+    "author": "System"
+}
+
+@on_data("extensions.check")
+async def ext_request(data: Data):
+    ...
